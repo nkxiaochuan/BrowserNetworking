f56efc2fa365d6c040a44e55aba4d30f8510cc8d
Andy <liuchuanshi3688@gmail.com>
2016-05-02 13:29:40 +0800

    commit

---
+++ src/main/java/com/network/browser/controller/XMLHttpRequestController.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.network.browser.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Andy
+ *
+ */
+@Controller
+public class XMLHttpRequestController {
+
+	@RequestMapping("/cors")
+	public void CORS(){
+		
+	}
+	@RequestMapping("/download")
+	public void download(){
+		
+	}
+	@RequestMapping("/upload")
+	public void upload(){
+		
+	}
+	@RequestMapping("/monitor")
+	public void monitor(){
+		
+	}
+	@RequestMapping("/streaming")
+	public void streaming(){
+		
+	}
+	@RequestMapping("/realtime")
+	public void realTime(){
+		
+	}
+	@RequestMapping("/polling")
+	public void polling(){
+		
+	}
+	@RequestMapping("/longpolling")
+	public void longPolling(){
+		
+	}
+}
